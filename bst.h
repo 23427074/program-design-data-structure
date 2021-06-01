03cd7c89bd3ad876daea8f39ef148ab818b59848
23427074 <77039165+23427074@users.noreply.github.com>
2021-06-01 20:52:02 +0800

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
 #ifndef BST_H
 #define BST_H
 
@@ -12,44 +8,7 @@ typedef struct tnode
     struct tnode *left;
 }Tnode;
 
-Tnode *buildbst(Tnode *makebst, int num, int *bstcnt)
-{
-    Tnode *current = makebst;
-    if(current==NULL)
-    {
-        Tnode *current = malloc(sizeof(Tnode));
-        current->bstnum = num;
-        current->right = NULL;
-        current->left = NULL;
-        (*bstcnt)++;
-        return current;
-    }
-    if(num>current->bstnum)
-    {
-        makebst->right = buildbst(makebst->right, num, bstcnt);
-    }
-    else if(num<current->bstnum)
-    {
-        makebst->left = buildbst(makebst->left, num, bstcnt);
-    }
-    return makebst;
-}
-
-void searchbst(Tnode *makebst, int tmp)
-{
-    if(makebst==NULL)return;
-    if(tmp<makebst->bstnum)
-    {
-        searchbst(makebst->left, tmp);
-    }
-    else if(tmp>makebst->bstnum)
-    {
-        searchbst(makebst->right, tmp);
-    }
-    else
-    {
-        return;
-    }
-}
+Tnode *buildbst(Tnode *makebst, int num, int *bstcnt);
+void searchbst(Tnode *makebst, int tmp);
 
 #endif
