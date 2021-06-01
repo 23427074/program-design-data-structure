2e4efa41983f076b439911b8daf2acc1b38d61c7
23427074 <77039165+23427074@users.noreply.github.com>
2021-06-01 20:52:23 +0800

    Update linklist.h

--- linklist.h
+++ linklist.h
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
 #ifndef LINKLIST_H
 #define LINKLIST_H
 
@@ -12,37 +8,7 @@ typedef struct node
 }Node;
 
 
-Node *buildlinked(Node *makell, int num, int *llcnt, int generate)
-{
-    if(makell == NULL)
-    {
-        Node *firstnode = malloc(sizeof(Node));
-        makell = firstnode;
-        firstnode->linknum = num;
-        makell->next = NULL;
-        (*llcnt)++;
-        return makell;
-    }
-    Node *newnode = malloc(sizeof(Node));
-    newnode->linknum = num;
-    newnode->next = makell;
-    makell = newnode;
-    (*llcnt)++;
-    return makell;
-}
-
-void searchlinked(Node *makell, int tmp)
-{
-    if(makell==NULL)return;
-    Node *current = makell;
-    while(current)
-    {
-        if((current->linknum)==tmp)
-        {
-            return;
-        }
-        current = current->next;
-    }
-}
+void buildlinked(Node **makell, int num);
+void searchlinked(Node **makell, int tmp);
 
 #endif
