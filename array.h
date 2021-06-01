246f06ad81a12eb620100cfb97dc5a1669d7768e
23427074 <77039165+23427074@users.noreply.github.com>
2021-06-01 20:50:37 +0800

    Update array.h

--- array.h
+++ array.h
@@ -1,28 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #ifndef ARRAY_H
 #define ARRAY_H
 
-void buildarray(int *str, int generate)
-{
-    int *array = malloc(sizeof(int)*generate);
-    for(int i=0;i<generate;i++)
-    {
-        *(array+i) = *(str+i);
-    }
-    free(array);
-}
-
-void searcharray(int *str, int len, int tmp)
-{
-    for(int i=0;i<len;i++)
-    {
-        if(*(str+i)==tmp)
-        {
-            return;
-        }
-    }
-}
+void buildarray(int *str, int generate);
+void searcharray(int *str, int len, int tmp);
 
 #endif
