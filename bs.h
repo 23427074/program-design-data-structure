e46ad68db666a7c66f240330300f37c1302004c0
23427074 <77039165+23427074@users.noreply.github.com>
2021-06-01 20:51:38 +0800

    Update bs.h

--- bs.h
+++ bs.h
@@ -1,35 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #ifndef BS_H
 #define BS_H
 
-int *buildbs(int *makebs, int *bsarray, int generate)
-{
-    for(int i=0;i<generate;i++)
-    {
-        *(bsarray+i) = *(makebs+i);
-    }
-    return bsarray;
-}
-
-void searchbs(int *bsarray, int len, int tmp)
-{
-    int left = 0;
-    int right = tmp - 1;
-    while(left<right)
-    {
-        int mid = (left+right)/2;
-        if(bsarray[mid] < len)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid + 1;
-        }
-    }
-    right++;
-}
+int *buildbs(int *makebs, int *bsarray, int generate);
+void searchbs(int *bsarray, int len, int tmp);
 
 #endif
